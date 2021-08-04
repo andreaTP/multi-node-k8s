5e758b31757c0dce71565eb1dfb9227c773d03ea
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2021-08-05 01:20:14 +0200

    changed gen of keys.

--- kubernetes/generateKeys.sh
+++ kubernetes/generateKeys.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-rm -rf hostKeys
-mkdir hostKeys
-ssh-keygen -q -t rsa  -f hostKeys/ssh_host_rsa_key -N "" -C ""
-cp hostKeys/ssh_host_rsa_key.pub ../container/authorized_keys
-cp hostKeys/ssh_host_rsa_key.pub ../container/id_rsa.pub
-cp hostKeys/ssh_host_rsa_key ../container/id_rsa
+rm -rf tmp_keys
+mkdir tmp_keys
+ssh-keygen -q -t rsa  -f tmp_keys/ssh_host_rsa_key -N "" -C ""
+cp tmp_keys/ssh_host_rsa_key.pub ../container/authorized_keys
+cp tmp_keys/ssh_host_rsa_key.pub ../container/id_rsa.pub
+cp tmp_keys/ssh_host_rsa_key ../container/id_rsa
+rm -rf tmp_keys
